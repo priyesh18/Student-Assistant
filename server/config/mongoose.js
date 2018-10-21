d545e6910821bc5028b3cc630729158664863a11
Priyesh Patel <safal.priyesh@gmail.com>
2018-10-21 10:38:17 +0530

    update database url

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ var config = require('./../config/config');
 //initially promises were developed by third party like bluebird but it became too famous and got add to js
 //telling mongoose to use promise
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL || config.database.mLab);
+mongoose.connect(process.env.MONGO_URL || config.database.local);
 
 mongoose.connection.on('connected', () => {
     console.log('connected to database');
